f5b758b9eb9b67f799c263a370d311286aa525fb
RyanHowe083 <ryan.howe2008@gmail.com>
2020-07-29 17:28:20 -0500

    parsing the url

--- PhotoAlbumProject/src/LTProject/Album.java
+++ PhotoAlbumProject/src/LTProject/Album.java
@@ -7,19 +7,31 @@ package LTProject;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.io.IOException;
 
-/**
- * This class creates a console application which displays photo ids and titles
- * in an album.
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Album {
 	
 	//A single URL connection
 	private static HttpURLConnection connection;
+	
+	
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws JSONException {
+		BufferedReader reader;
+		StringBuffer responseContent = new StringBuffer();
+		String line;
+		JSONObject json = new JSONObject();
+		
+		
+		
+		
+		
 		try {
 			//Set the URL for the json placeholder
 			URL url = new URL("https://jsonplaceholder.typicode.com/photos");
@@ -28,12 +40,43 @@ public class Album {
 			//Get request
 			connection.setRequestMethod("GET");
 			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			
+			int status = connection.getResponseCode();
+			
+			if(status > 299) {
+				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+					
+					
+				}
+				reader.close();
+				
+			}else {
+				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+					String[] temp = line.split(":");
+					
+					
+//					System.out.println(temp.length);
+				}
+				reader.close();
 
+			}
+			
+			
+			
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
 		}
 
 	}
