e606ab11d15696bf2032aa7e5b145a8a2fd58eea
RyanHowe083 <ryan.howe2008@gmail.com>
2020-07-29 13:38:06 -0500

    Added try catch

--- PhotoAlbumProject/src/LTProject/Album.java
+++ PhotoAlbumProject/src/LTProject/Album.java
@@ -1,15 +1,33 @@
 package LTProject;
 
 /**
- * 
  * @author ryanhowe
- *
+ */
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.IOException;
+
+/**
+ * This class creates a console application which displays photo ids and titles
+ * in an album.
  */
 public class Album {
-	
-	/**
-	 * This class creates a console application 
-	 * which displays photo ids and titles in an album.
-	 */
 
+	private static HttpURLConnection connection;
+
+	public static void main(String[] args) {
+
+		try {
+			URL url = new URL("https://jsonplaceholder.typicode.com/photos");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
