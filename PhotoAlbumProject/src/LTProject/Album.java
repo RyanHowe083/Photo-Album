85005181341d6622ac80cfa34bf6e1eebf5035eb
RyanHowe083 <ryan.howe2008@gmail.com>
2020-07-28 19:37:07 -0500

    Adding source folder

---
+++ PhotoAlbumProject/src/LTProject/Album.java
@@ -0,0 +1,15 @@
+package LTProject;
+
+/**
+ * 
+ * @author ryanhowe
+ *
+ */
+public class Album {
+	
+	/**
+	 * This class creates a console application 
+	 * which displays photo ids and titles in an album.
+	 */
+
+}
